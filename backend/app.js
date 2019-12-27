b188191ec8e752a13b03d5bb33072f20a9c93ad4
Axis <atonymartin20@yahoo.com>
2019-12-26 21:58:54 -0600

    Edited backend app.js and created dbConfig.js

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,14 @@ const app = express();
 
 // Add Routers
 const catcherRouter = require('./routers/catcherRouter');
+const firstBaseRouter = require('./routers/firstBaseRouter');
+const secondBaseRouter = require('./routers/secondBaseRouter');
+const shortStopRouter = require('./routers/shortStopRouter');
+const thirdBaseRouter = require('./routers/thirdBaseRouter');
+const outfieldRouter = require('./routers/outfieldRouter');
+const startingPitcherRouter = require('./routers/startingPitcherRouter');
+const reliefPitcherRouter = require('./routers/reliefPitcherRouter');
+const designatedHitterRouter = require('./routers/designatedHitterRouter');
 
 const cors = require('cors');
 
@@ -13,6 +21,14 @@ app.use(cors());
 const PORT = process.env.PORT || '6987';
 
 app.use('/api/catchers', catcherRouter);
+app.use('/api/firstBase', firstBaseRouter);
+app.use('/api/secondBase', secondBaseRouter);
+app.use('/api/shortStop', shortStopRouter);
+app.use('/api/thirdBase', thirdBaseRouter);
+app.use('/api/outfield', outfieldRouter);
+app.use('/api/startingPitcher', startingPitcherRouter);
+app.use('/api/reliefPitcher', reliefPitcherRouter);
+app.use('/api/designatedHitter', designatedHitterRouter);
 
 
 app.get('/', (req, res) => {
