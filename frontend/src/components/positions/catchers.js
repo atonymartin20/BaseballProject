5e4ed91b6197e5cf3ebef6285c904e2f03e74431
Axis <atonymartin20@yahoo.com>
2020-01-05 20:46:20 -0600

    Testing player Table design

--- frontend/src/components/positions/catchers.js
+++ frontend/src/components/positions/catchers.js
@@ -7,28 +7,7 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import { AppContext } from '../context/appContext.js';
 import Grid from '@material-ui/core/Grid';
-
-// import PropTypes from 'prop-types';
-// import clsx from 'clsx';
-// import { lighten, makeStyles } from '@material-ui/core/styles';
-// import Table from '@material-ui/core/Table';
-// import TableBody from '@material-ui/core/TableBody';
-// import TableCell from '@material-ui/core/TableCell';
-// import TableContainer from '@material-ui/core/TableContainer';
-// import TableHead from '@material-ui/core/TableHead';
-// import TablePagination from '@material-ui/core/TablePagination';
-// import TableRow from '@material-ui/core/TableRow';
-// import TableSortLabel from '@material-ui/core/TableSortLabel';
-// import Toolbar from '@material-ui/core/Toolbar';
-// import Typography from '@material-ui/core/Typography';
-// import Paper from '@material-ui/core/Paper';
-// import Checkbox from '@material-ui/core/Checkbox';
-// import IconButton from '@material-ui/core/IconButton';
-// import Tooltip from '@material-ui/core/Tooltip';
-// import FormControlLabel from '@material-ui/core/FormControlLabel';
-// import Switch from '@material-ui/core/Switch';
-// import DeleteIcon from '@material-ui/icons/Delete';
-// import FilterListIcon from '@material-ui/icons/FilterList';
+import PlayerTable from './playerTable.js';
 
 const styles = theme => ({
     flipDataList: {
@@ -149,6 +128,7 @@ class Catchers extends React.Component {
                 {display2019 ? (
                     <div>
                         <h1 className={classes.positionText}>2019 Data here</h1>
+                        <PlayerTable />
                     </div>) : null
                 }
 
