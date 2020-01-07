6e8f44ed34cc2249a13fe03aef65f78aa944bae0
Axis <atonymartin20@yahoo.com>
2020-01-06 19:49:05 -0600

    Cleaned up enhancedTable.js code

--- frontend/src/components/positionTable/enhandedTable.js
+++ frontend/src/components/positionTable/enhandedTable.js
@@ -15,22 +15,6 @@ function createData(name, PAs, AVG, OBP, HR, runs, RBIs, SBs) {
     return { name, PAs, AVG, OBP, HR, runs, RBIs, SBs };
 }
 
-// let rows = [
-//     createData('Cupcake', 305, 3.7, 67, 4.3, 7, 7, 7),
-//     createData('Donut', 452, 25.0, 51, 4.9),
-//     createData('Eclair', 262, 16.0, 24, 6.0),
-//     createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-//     createData('Gingerbread', 356, 16.0, 49, 3.9),
-//     createData('Honeycomb', 408, 3.2, 87, 6.5),
-//     createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-//     createData('Jelly Bean', 375, 0.0, 94, 0.0),
-//     createData('KitKat', 518, 26.0, 65, 7.0),
-//     createData('Lollipop', 392, 0.2, 98, 0.0),
-//     createData('Marshmallow', 318, 0, 81, 2.0),
-//     createData('Nougat', 360, 19.0, 9, 37.0),
-//     createData('Oreo', 437, 18.0, 63, 4.0),
-// ];
-
 function desc(a, b, orderBy) {
     if (b[orderBy] < a[orderBy]) {
         return -1;
@@ -143,7 +127,7 @@ export default function EnhancedTable(props) {
         if (props.players) {
             console.log(props.players)
             setRows(props.players.map((player, index) => (
-                createData(`${player.firstName} ${player.lastName}`, player.SteamerPAProjection, `${player.SteamerAVGProjection}`, `${player.SteamerOBPProjection}`, `${player.SteamerHRProjection}`)
+                createData(`${player.firstName} ${player.lastName}`, player.SteamerPAProjection, player.SteamerAVGProjection, player.SteamerOBPProjection, player.SteamerHRProjection)
             )))
         }
         else {
@@ -164,17 +148,6 @@ export default function EnhancedTable(props) {
             ])
         }
     }, [props.players])
-    // const findRows = () => {
-    //     if (props.players) {
-    //         let rows = this.props.players.map((player, index) => (
-    //             createData(`${player.firstName} ${player.lastName}`, `${player.SteamerPAProjection}`, `${player.SteamerAVGProjection}`, `${player.SteamerOBPProjection}`, `${player.SteamerHRProjection}`)
-    //         ))
-    //         console.log(rows)
-    //     }
-    //     else {
-
-    //     }
-    // }
 
     const handleRequestSort = (event, property) => {
         const isAsc = orderBy === property && order === 'asc';
