1eb22bd34464b8a5387adedb98aea08e332e22c5
Axis <atonymartin20@yahoo.com>
2020-02-01 22:45:10 -0600

    Grabbing all positional data in context now

--- frontend/src/components/context/appContext.js
+++ frontend/src/components/context/appContext.js
@@ -37,6 +37,126 @@ export default class AppProvider extends Component {
                                 console.log('error getting catchers', err)
                             });
                     },
+                    getFirstBase: () => {
+                        const endpoint = '/firstBase';
+                        axios
+                            .get(endpoint)
+                            .then(res => {
+                                const firstBase = res.data;
+                                localStorage.setItem('firstBase', JSON.stringify(firstBase));
+                                this.setState({
+                                    firstBase
+                                });
+                            })
+                            .catch(err => {
+                                console.log('error getting firstBase', err)
+                            });
+                    },
+                    getSecondBase: () => {
+                        const endpoint = '/secondBase';
+                        axios
+                            .get(endpoint)
+                            .then(res => {
+                                const secondBase = res.data;
+                                localStorage.setItem('secondBase', JSON.stringify(secondBase));
+                                this.setState({
+                                    secondBase
+                                });
+                            })
+                            .catch(err => {
+                                console.log('error getting secondBase', err)
+                            });
+                    },
+                    getThirdBase: () => {
+                        const endpoint = '/thirdBase';
+                        axios
+                            .get(endpoint)
+                            .then(res => {
+                                const thirdBase = res.data;
+                                localStorage.setItem('thirdBase', JSON.stringify(thirdBase));
+                                this.setState({
+                                    thirdBase
+                                });
+                            })
+                            .catch(err => {
+                                console.log('error getting thirdBase', err)
+                            });
+                    },
+                    getShortStop: () => {
+                        const endpoint = '/shortStop';
+                        axios
+                            .get(endpoint)
+                            .then(res => {
+                                const shortStop = res.data;
+                                localStorage.setItem('shortStop', JSON.stringify(shortStop));
+                                this.setState({
+                                    shortStop
+                                });
+                            })
+                            .catch(err => {
+                                console.log('error getting shortStop', err)
+                            });
+                    },
+                    getOutfield: () => {
+                        const endpoint = '/outfield';
+                        axios
+                            .get(endpoint)
+                            .then(res => {
+                                const outfield = res.data;
+                                localStorage.setItem('outfield', JSON.stringify(outfield));
+                                this.setState({
+                                    outfield
+                                });
+                            })
+                            .catch(err => {
+                                console.log('error getting outfield', err)
+                            });
+                    },
+                    getDesignatedHitters: () => {
+                        const endpoint = '/designatedHitter';
+                        axios
+                            .get(endpoint)
+                            .then(res => {
+                                const designatedHitters = res.data;
+                                localStorage.setItem('designatedHitters', JSON.stringify(designatedHitters));
+                                this.setState({
+                                    designatedHitters
+                                });
+                            })
+                            .catch(err => {
+                                console.log('error getting designatedHitters', err)
+                            });
+                    },
+                    getStartingPitchers: () => {
+                        const endpoint = '/startingPitchers';
+                        axios
+                            .get(endpoint)
+                            .then(res => {
+                                const startingPitchers = res.data;
+                                localStorage.setItem('startingPitchers', JSON.stringify(startingPitchers));
+                                this.setState({
+                                    startingPitchers
+                                });
+                            })
+                            .catch(err => {
+                                console.log('error getting startingPitchers', err)
+                            });
+                    },
+                    getReliefPitchers: () => {
+                        const endpoint = '/reliefPitchers';
+                        axios
+                            .get(endpoint)
+                            .then(res => {
+                                const reliefPitchers = res.data;
+                                localStorage.setItem('reliefPitchers', JSON.stringify(reliefPitchers));
+                                this.setState({
+                                    reliefPitchers
+                                });
+                            })
+                            .catch(err => {
+                                console.log('error getting reliefPitchers', err)
+                            });
+                    },
                 }}
             >
                 {this.props.children}
