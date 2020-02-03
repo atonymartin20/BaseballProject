01fb364bfaa1a6f6548dc6167f7df3c4a3ddcd38
Axis <atonymartin20@yahoo.com>
2020-02-03 09:41:26 -0600

    Fixing server errors

--- backend/routers/startingPitcherRouter.js
+++ backend/routers/startingPitcherRouter.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
     startingPitcherModel
-        .getStartingPitcher()
+        .getStartingPitchers()
         .then(startingPitcher => {
             res.json(startingPitcher);
         })
