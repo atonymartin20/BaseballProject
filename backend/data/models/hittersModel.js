f781e641abc0afe0768450befbb61e6c5aeb4052
Axis <atonymartin20@yahoo.com>
2020-04-18 13:08:05 -0500

    more backend testing

--- backend/data/models/hittersModel.js
+++ backend/data/models/hittersModel.js
@@ -4,11 +4,7 @@ module.exports = {
     findByID: id => {
         return db('hitters').where({ id }).first();
     },
-    // getHitters() {
-    //     return db('hitters');
-    // }
     getHitters() {
-        db('hitters').joinRaw('natural full join catchers')
         return db('hitters');
     }
 }
