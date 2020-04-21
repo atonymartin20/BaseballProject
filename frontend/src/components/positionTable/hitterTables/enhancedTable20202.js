0d98da93b3d64fc708be09c6cc29976207e12e59
Axis <atonymartin20@yahoo.com>
2020-04-21 16:48:49 -0500

    More testing

--- frontend/src/components/positionTable/hitterTables/enhancedTable20202.js
+++ frontend/src/components/positionTable/hitterTables/enhancedTable20202.js
@@ -143,33 +143,36 @@ export default function EnhancedTable(props) {
     const [playerCard, setPlayerCard] = React.useState(false);
     const [rows, setRows] = React.useState([]);
     const globalState = useContext(AppContext)
-    console.log(globalState)
 
     React.useEffect(() => {
+        console.log(rows.length)
         if (props.players.length !== 0) {
             setRows(props.players.map((player, index) => (
                 createData(`${player.firstName} ${player.lastName}`, player.SteamerPAProjection, Number(player.SteamerAVGProjection), Number(player.SteamerOBPProjection), player.SteamerHRProjection, player.SteamerRunsProjection, player.SteamerRBIProjection, player.SteamerSBProjection, Number(player.SteamerFWARProjection), ((player.SteamerRunsProjection + player.SteamerRBIProjection + (6 * player.SteamerHRProjection) + (6.5 * player.SteamerSBProjection) + ((player.SteamerPAProjection * player.SteamerAVGProjection))) / 6), ((player.SteamerRunsProjection + player.SteamerRBIProjection + (6 * player.SteamerHRProjection) + (6.5 * player.SteamerSBProjection) + ((player.SteamerPAProjection * player.SteamerOBPProjection))) / 6),player.id, index)
             )))
         }
         else {
-            globalState.getHitters();
-            globalState.getPitchers();
-            globalState.getCatchers();
-            globalState.getFirstBase();
-            globalState.getSecondBase();
-            globalState.getShortStop();
-            globalState.getThirdBase();
-            globalState.getMiddleInfield();
-            globalState.getCornerInfield();
-            globalState.getOutfield();
-            globalState.getDesignatedHitters();
-            globalState.getStartingPitchers();
-            globalState.getReliefPitchers();
-            setTimeout(() => {
-                setRows(props.players.map((player, index) => (
-                    createData(`${player.firstName} ${player.lastName}`, player.SteamerPAProjection, Number(player.SteamerAVGProjection), Number(player.SteamerOBPProjection), player.SteamerHRProjection, player.SteamerRunsProjection, player.SteamerRBIProjection, player.SteamerSBProjection, Number(player.SteamerFWARProjection), ((player.SteamerRunsProjection + player.SteamerRBIProjection + (6 * player.SteamerHRProjection) + (6.5 * player.SteamerSBProjection) + ((player.SteamerPAProjection * player.SteamerAVGProjection))) / 6), ((player.SteamerRunsProjection + player.SteamerRBIProjection + (6 * player.SteamerHRProjection) + (6.5 * player.SteamerSBProjection) + ((player.SteamerPAProjection * player.SteamerOBPProjection))) / 6),player.id, index)
-                )))
-            }, 1000)
+            console.log(rows.length)
+            if(props.players.length === 0) {
+                globalState.getHitters();
+                globalState.getPitchers();
+                globalState.getCatchers();
+                globalState.getFirstBase();
+                globalState.getSecondBase();
+                globalState.getShortStop();
+                globalState.getThirdBase();
+                globalState.getMiddleInfield();
+                globalState.getCornerInfield();
+                globalState.getOutfield();
+                globalState.getDesignatedHitters();
+                globalState.getStartingPitchers();
+                globalState.getReliefPitchers();
+                setTimeout(() => {
+                    setRows(props.players.map((player, index) => (
+                        createData(`${player.firstName} ${player.lastName}`, player.SteamerPAProjection, Number(player.SteamerAVGProjection), Number(player.SteamerOBPProjection), player.SteamerHRProjection, player.SteamerRunsProjection, player.SteamerRBIProjection, player.SteamerSBProjection, Number(player.SteamerFWARProjection), ((player.SteamerRunsProjection + player.SteamerRBIProjection + (6 * player.SteamerHRProjection) + (6.5 * player.SteamerSBProjection) + ((player.SteamerPAProjection * player.SteamerAVGProjection))) / 6), ((player.SteamerRunsProjection + player.SteamerRBIProjection + (6 * player.SteamerHRProjection) + (6.5 * player.SteamerSBProjection) + ((player.SteamerPAProjection * player.SteamerOBPProjection))) / 6),player.id, index)
+                    )))
+                }, 1000)
+            }
         }
     }, [props.players])
 
