c20b409efe7d9d2e6e6eb8045b566660c95f66a3
Alex Martin <atonymartin20@yahoo.com>
2021-04-02 22:51:10 -0500

    Benardo Flores Jr RP to Cardinals

--- backend/data/seeds/00_07_reliefPitchers.js
+++ backend/data/seeds/00_07_reliefPitchers.js
@@ -35430,7 +35430,7 @@ exports.seed = function (knex, Promise) {
             'birthday': '8/23/1995',
             'bats': 'L',
             'throws': 'L',
-            'currentTeam': 'White Sox',
+            'currentTeam': 'Cardinals',
             'imageURL': 'https://www.baseball-reference.com/req/202007270/images/headshots/5/57f3b0b2_mlbam.jpg',
             'primaryPosition': 'RP',
             'otherPositions': '',
