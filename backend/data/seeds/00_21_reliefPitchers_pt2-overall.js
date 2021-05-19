0e03ba2a033de94f7358099faf287e9b69c696ce
Alex Martin <atonymartin20@yahoo.com>
2021-05-18 22:44:24 -0500

    Noe Ramirez RP now FA

--- backend/data/seeds/00_21_reliefPitchers_pt2-overall.js
+++ backend/data/seeds/00_21_reliefPitchers_pt2-overall.js
@@ -51950,7 +51950,7 @@ exports.seed = function (knex, Promise) {
             'birthday': '12/22/1989',
             'bats': 'R',
             'throws': 'R',
-            'currentTeam': 'Angels',
+            'currentTeam': 'Free Agent',
             'imageURL': 'https://d3k2oh6evki4b7.cloudfront.net/req/202001270/images/headshots/a/ad4d906f_mlbam.jpg',
             'primaryPosition': 'RP',
             'otherPositions': '',
