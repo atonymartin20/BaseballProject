9174a3ce8f575d37da5cf4dc27449fbb385ac538
Alex Martin <atonymartin20@yahoo.com>
2021-05-26 22:39:52 -0500

    Dee Strange-Gordon OF signs with Cubs

--- backend/data/seeds/00_14_outfield-overall.js
+++ backend/data/seeds/00_14_outfield-overall.js
@@ -37498,7 +37498,7 @@ exports.seed = function (knex, Promise) {
             'birthday': '4/22/1988',
             'bats': 'L',
             'throws': 'R',
-            'currentTeam': 'Free Agent',
+            'currentTeam': 'Cubs',
             'imageURL': 'https://d3k2oh6evki4b7.cloudfront.net/req/202001270/images/headshots/7/709a21a2_mlbam.jpg',
             'primaryPosition': 'OF',
             'otherPositions': '2B',
