487740c8ec6051cf5917a79684ce35a9c629a2f9
Alex Martin <atonymartin20@yahoo.com>
2021-06-18 23:40:41 -0500

    Mike Ford 1B traded to Rays

--- backend/data/seeds/00_10_firstBase-overall.js
+++ backend/data/seeds/00_10_firstBase-overall.js
@@ -4332,7 +4332,7 @@ exports.seed = function (knex, Promise) {
             'birthday': '7/4/1992',
             'bats': 'L',
             'throws': 'R',
-            'currentTeam': 'Yankees',
+            'currentTeam': 'Rays',
             'imageURL': 'https://d3k2oh6evki4b7.cloudfront.net/req/202001270/images/headshots/e/ee192a64_mlbam.jpg',
             'primaryPosition': '1B',
             'otherPositions': '',
