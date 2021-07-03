5225d46fbb5fce49b4490a1bc7e8252ecdf170a6
Alex Martin <atonymartin20@yahoo.com>
2021-07-02 23:58:47 -0500

    Cam Bedrosian RP now FA

--- backend/data/seeds/00_19_reliefPitchers-overall.js
+++ backend/data/seeds/00_19_reliefPitchers-overall.js
@@ -11482,7 +11482,7 @@ exports.seed = function (knex, Promise) {
             'birthday': '10/2/1991',
             'bats': 'R',
             'throws': 'R',
-            'currentTeam': 'Athletics',
+            'currentTeam': 'Free Agent',
             'imageURL': 'https://d3k2oh6evki4b7.cloudfront.net/req/202001270/images/headshots/0/0616b38f_mlbam.jpg',
             'primaryPosition': 'RP',
             'otherPositions': '',
