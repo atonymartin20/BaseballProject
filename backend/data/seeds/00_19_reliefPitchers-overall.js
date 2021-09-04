4a18ecb9c4aa177e30c622ea32cef6c4b1cf50a4
Alex Martin <atonymartin20@yahoo.com>
2021-09-03 20:42:15 -0500

    RP Buck Farmer joins Rangers

--- backend/data/seeds/00_19_reliefPitchers-overall.js
+++ backend/data/seeds/00_19_reliefPitchers-overall.js
@@ -40776,7 +40776,7 @@ exports.seed = function (knex, Promise) {
             'birthday': '2/20/1991',
             'bats': 'L',
             'throws': 'R',
-            'currentTeam': 'Free Agent',
+            'currentTeam': 'Rangers',
             'imageURL': 'https://d3k2oh6evki4b7.cloudfront.net/req/202001270/images/headshots/c/c5993341_mlbam.jpg',
             'primaryPosition': 'RP',
             'otherPositions': '',
