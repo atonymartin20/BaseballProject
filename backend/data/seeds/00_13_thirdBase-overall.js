1e509e544b0f670250b8fbd0e267b0b985cee2cd
Alex Martin <atonymartin20@yahoo.com>
2021-09-11 23:22:50 -0500

    3B Jake Lamb to Blue Jays

--- backend/data/seeds/00_13_thirdBase-overall.js
+++ backend/data/seeds/00_13_thirdBase-overall.js
@@ -5980,7 +5980,7 @@ exports.seed = function (knex, Promise) {
             'birthday': '10/9/1990',
             'bats': 'L',
             'throws': 'R',
-            'currentTeam': 'White Sox',
+            'currentTeam': 'Blue Jays',
             'imageURL': 'https://d3k2oh6evki4b7.cloudfront.net/req/202001270/images/headshots/2/29f8b660_mlbam.jpg',
             'primaryPosition': '3B',
             'otherPositions': '1B',
