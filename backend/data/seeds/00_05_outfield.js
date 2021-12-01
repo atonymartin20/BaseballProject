402272450feb77b691ec5ac36af20809dd63c750
Alex Martin <atonymartin20@yahoo.com>
2021-12-01 17:47:24 -0600

    Alex Dickerson OF now FA

--- backend/data/seeds/00_05_outfield.js
+++ backend/data/seeds/00_05_outfield.js
@@ -14343,7 +14343,7 @@ exports.seed = function (knex, Promise) {
             'birthday': '5/26/1990',
             'bats': 'L',
             'throws': 'L',
-            'currentTeam': 'Giants',
+            'currentTeam': 'Free Agent',
             'imageURL': 'https://d3k2oh6evki4b7.cloudfront.net/req/202001270/images/headshots/e/ebfce0cf_mlbam.jpg',
             'primaryPosition': 'OF',
             'otherPositions': '',
