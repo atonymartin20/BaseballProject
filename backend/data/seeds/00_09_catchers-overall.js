1386ace323a2a271faafeee3fed655b5d14dd762
Alex Martin <atonymartin20@yahoo.com>
2021-12-04 18:47:11 -0600

    Aramis Garcia C signs with Reds

--- backend/data/seeds/00_09_catchers-overall.js
+++ backend/data/seeds/00_09_catchers-overall.js
@@ -5838,7 +5838,7 @@ exports.seed = function (knex, Promise) {
             'birthday': '1/12/1993',
             'bats': 'R',
             'throws': 'R',
-            'currentTeam': 'Free Agent',
+            'currentTeam': 'Reds',
             'imageURL': 'https://www.baseball-reference.com/req/202011060/images/headshots/c/ce8ff2cd_mlbam.jpg',
             'primaryPosition': 'C',
             'otherPositions': '',
