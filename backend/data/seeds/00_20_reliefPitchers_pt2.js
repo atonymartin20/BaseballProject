a2faeeaa5ac65ceb17c11599d881c3dde4c7241d
Alex Martin <atonymartin20@yahoo.com>
2021-12-23 20:36:04 -0600

    Brooks Kriske RP signs in Japan

--- backend/data/seeds/00_20_reliefPitchers_pt2.js
+++ backend/data/seeds/00_20_reliefPitchers_pt2.js
@@ -58416,7 +58416,7 @@ exports.seed = function (knex, Promise) {
             'birthday': '2/3/1994',
             'bats': 'R',
             'throws': 'R',
-            'currentTeam': 'Orioles',
+            'currentTeam': 'Japan',
             'imageURL': 'https://www.baseball-reference.com/req/202007270/images/headshots/1/103b9069_mlbam.jpg',
             'primaryPosition': 'RP',
             'otherPositions': '',
