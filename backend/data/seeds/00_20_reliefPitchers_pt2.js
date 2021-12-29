1638f5ecacb833bbed43eda6deec400cf5bd675e
Alex Martin <atonymartin20@yahoo.com>
2021-12-28 20:11:13 -0600

    Robert Gsellman RP now FA

--- backend/data/seeds/00_20_reliefPitchers_pt2.js
+++ backend/data/seeds/00_20_reliefPitchers_pt2.js
@@ -23690,7 +23690,7 @@ exports.seed = function (knex, Promise) {
             'birthday': '7/18/1993',
             'bats': 'R',
             'throws': 'R',
-            'currentTeam': 'Mets',
+            'currentTeam': 'Free Agent',
             'imageURL': 'https://d3k2oh6evki4b7.cloudfront.net/req/202001270/images/headshots/3/348ef7be_mlbam.jpg',
             'primaryPosition': 'RP',
             'otherPositions': '',
