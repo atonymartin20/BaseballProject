8f8ceb1b3f4fddc89367c450b3f1e53c2199c614
Alex Martin <atonymartin20@yahoo.com>
2022-01-12 21:15:40 -0600

    Tyler Chatwood RP signs in Japan

--- backend/data/seeds/00_19_reliefPitchers-overall.js
+++ backend/data/seeds/00_19_reliefPitchers-overall.js
@@ -40460,7 +40460,7 @@ exports.seed = function (knex, Promise) {
             'birthday': '12/16/1989',
             'bats': 'R',
             'throws': 'R',
-            'currentTeam': 'Free Agent',
+            'currentTeam': 'Japan',
             'imageURL': 'https://d3k2oh6evki4b7.cloudfront.net/req/202001270/images/headshots/0/0f6cd2cd_mlbam.jpg',
             'primaryPosition': 'RP',
             'otherPositions': '',
