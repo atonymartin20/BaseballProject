c190d4bf2185f5a6e5c1d5996fc8fab236d5e756
Alex Martin <atonymartin20@yahoo.com>
2022-01-12 21:15:52 -0600

    Gregory Polanco OF signs in Japan

--- backend/data/seeds/00_26_outfield_pt2.js
+++ backend/data/seeds/00_26_outfield_pt2.js
@@ -6,7 +6,7 @@ exports.seed = function (knex, Promise) {
             'birthday': '9/14/1991',
             'bats': 'L',
             'throws': 'L',
-            'currentTeam': 'Free Agent',
+            'currentTeam': 'Japan',
             'imageURL': 'https://d3k2oh6evki4b7.cloudfront.net/req/202001270/images/headshots/e/e1a6db65_mlbam.jpg',
             'primaryPosition': 'OF',
             'otherPositions': '',
