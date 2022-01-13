f11c6a94c5558b49ccf08c490d75cc96246c6fd3
Alex Martin <atonymartin20@yahoo.com>
2022-01-12 21:14:52 -0600

    Rio Ruiz 2B signs in Korea

--- backend/data/seeds/00_02_secondBase.js
+++ backend/data/seeds/00_02_secondBase.js
@@ -17222,7 +17222,7 @@ exports.seed = function (knex, Promise) {
             'birthday': '5/22/1994',
             'bats': 'L',
             'throws': 'R',
-            'currentTeam': 'Free Agent',
+            'currentTeam': 'Korea',
             'imageURL': 'https://d3k2oh6evki4b7.cloudfront.net/req/202001270/images/headshots/8/8080aa83_mlbam.jpg',
             'primaryPosition': '2B',
             'otherPositions': '',
