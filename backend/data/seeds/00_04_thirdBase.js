ff008e7b53e03f9a106744fe93fe4f900647b56d
Alex Martin <atonymartin20@yahoo.com>
2022-02-09 15:22:55 -0600

    Yolmer Sanchez 3B signs with Red Sox

--- backend/data/seeds/00_04_thirdBase.js
+++ backend/data/seeds/00_04_thirdBase.js
@@ -13857,7 +13857,7 @@ exports.seed = function (knex, Promise) {
             'birthday': '6/29/1992',
             'bats': 'S',
             'throws': 'R',
-            'currentTeam': 'Free Agent',
+            'currentTeam': 'Red Sox',
             'imageURL': 'https://d3k2oh6evki4b7.cloudfront.net/req/202001270/images/headshots/2/28b3d53d_mlbam.jpg',
             'primaryPosition': '3B',
             'otherPositions': '',
