6e2ec9a02dd9178fd3af76d6964765f9a7d9ffdd
Alex Martin <atonymartin20@yahoo.com>
2022-03-03 15:57:12 -0600

    RP Kevin McCarthy signs with Cubs

--- backend/data/seeds/00_23_reliefPitchers_pt3-overall.js
+++ backend/data/seeds/00_23_reliefPitchers_pt3-overall.js
@@ -11820,7 +11820,7 @@ exports.seed = function (knex, Promise) {
             'birthday': '2/22/1992',
             'bats': 'R',
             'throws': 'R',
-            'currentTeam': 'Free Agent',
+            'currentTeam': 'Cubs',
             'imageURL': 'https://d3k2oh6evki4b7.cloudfront.net/req/202001270/images/headshots/a/a560da42_mlbam.jpg',
             'primaryPosition': 'RP',
             'otherPositions': '',
