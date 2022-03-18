9c4b486bbf83135d70c131673c79180eab95dd29
Alex Martin <atonymartin20@yahoo.com>
2022-03-18 06:16:30 -0500

    SP Chris Bassitt traded to Mets

--- backend/data/seeds/00_18_startingPitchers-overall.js
+++ backend/data/seeds/00_18_startingPitchers-overall.js
@@ -7166,7 +7166,7 @@ exports.seed = function (knex, Promise) {
             'birthday': '2/22/1989',
             'bats': 'R',
             'throws': 'R',
-            'currentTeam': 'Athletics',
+            'currentTeam': 'Mets',
             'imageURL': 'https://d3k2oh6evki4b7.cloudfront.net/req/202001270/images/headshots/e/e54001ef_mlbam.jpg',
             'primaryPosition': 'SP',
             'otherPositions': '',
