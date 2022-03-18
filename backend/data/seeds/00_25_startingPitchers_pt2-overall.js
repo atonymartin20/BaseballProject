81583692cbb3f7e76199fc2e4d6b4cc5aa146b28
Alex Martin <atonymartin20@yahoo.com>
2022-03-17 22:11:46 -0500

    SP Vince Velasquez signs with White Sox

--- backend/data/seeds/00_25_startingPitchers_pt2-overall.js
+++ backend/data/seeds/00_25_startingPitchers_pt2-overall.js
@@ -38670,7 +38670,7 @@ exports.seed = function (knex, Promise) {
 			'birthday': '6/7/1992',
 			'bats': 'R',
 			'throws': 'R',
-			'currentTeam': 'Free Agent',
+            'currentTeam': 'White Sox',
 			'imageURL': 'https://d3k2oh6evki4b7.cloudfront.net/req/202001270/images/headshots/c/ca619690_mlbam.jpg',
 			'primaryPosition': 'SP',
 			'otherPositions': '',
